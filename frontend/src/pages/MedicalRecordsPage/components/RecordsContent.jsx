1ae0a2ad47d2e410db1ca36cc6074afadc28ca5b
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-04-27 15:59:03 +0530

    Frontend: Decouple Medical Records page into modular components

---
+++ frontend/src/pages/MedicalRecordsPage/components/RecordsContent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Loader2 } from 'lucide-react';
+import RecordsTimeline from './RecordsTimeline';
+import RecordDetailCard from './RecordDetailCard';
+
+const RecordsContent = ({ loading, records, selectedId, setSelectedId, selectedRecord }) => {
+  if (loading) {
+    return (
+      <div className="flex-1 flex flex-col items-center justify-center gap-4">
+        <Loader2 className="animate-spin text-[#2A7FFF]" size={40} />
+        <p className="text-gray-400 font-bold">Synchronizing medical vault...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-5 pb-4">
+      {/* Left Column: Timeline */}
+      <div className="w-full lg:w-[320px] shrink-0">
+        <RecordsTimeline 
+          records={records}
+          selectedId={selectedId} 
+          onSelect={setSelectedId} 
+        />
+      </div>
+
+      {/* Right Column: Detail View */}
+      <div className="flex-1 min-w-0">
+        <RecordDetailCard record={selectedRecord} />
+      </div>
+    </div>
+  );
+};
+
+export default RecordsContent;
